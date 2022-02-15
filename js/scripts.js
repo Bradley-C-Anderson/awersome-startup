6a28660ff8afc234d75d8cb5afed2562c08bd27b
Bradley-C-Anderson <97475790+Bradley-C-Anderson@users.noreply.github.com>
2022-02-15 16:54:00 -0600

    Employee data done
    Fetched and put into object array.

--- js/scripts.js
+++ js/scripts.js
@@ -14,19 +14,40 @@ function checkStatus(response) {
     }
   }
 
-fetchData('https://randomuser.me/api/?nat=us')
-    .then(data => getPersonData(data))
+fetchData('https://randomuser.me/api/?nat=us&results=12')
+    .then(data => createEmployeeCards(data.results))
+    
 
 function getPersonData(data){
-    const person = data.results[0];
-    const firstName = person.name.first;
-    const lastName = person.name.last;
-    const email = person.email;
-    const streetNumber = person.location.street.number;
-    const streetName = person.location.street.name;
-    const city = person.location.city;
-    const state = person.location.state;
-    const zip = person.location.postcode;
-    const birthday = `${person.dob.date.substring(5,7)}/${person.dob.date.substring(8,10)}/${person.dob.date.substring(0,4)}`
-    console.log(person.dob.date.substring(0,4));
+    employees = [];
+    for(let i = 0; i < data.length; i++){
+        const person = data[i];
+        const personData = {
+            gender : person.gender,
+            firstName : person.name.first,
+            lastName : person.name.last,
+            email : person.email,
+            streetNumber : person.location.street.number,
+            streetName : person.location.street.name,
+            city : person.location.city,
+            state : person.location.state,
+            zip : person.location.postcode,
+            birthday : `${person.dob.date.substring(5,7)}/${person.dob.date.substring(8,10)}/${person.dob.date.substring(0,4)}`
+        };
+        employees.push(personData);
+        
+    }
+    return employees;
 }
+
+function createEmployeeCards(data){
+    const employees = getPersonData(data);
+    
+    for(let i = 0; i < employees.length; i++){
+        console.log(employees[i].gender);
+    }
+
+    const gallery = document.getElementById('gallery');
+
+}
+
